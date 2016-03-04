108560b7e1618c6d467f2033f0cbdfb3f6c4ebae
Frederic Mallet <frederic.mallet@backelite.com>
2016-03-04 10:40:27 +0100

    Add new template (Converter)

---
+++ Backelite/Converter/Converter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
